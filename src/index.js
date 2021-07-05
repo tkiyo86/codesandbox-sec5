4d12329db894e08172c4718808fd85af242f1454
tkiyo86 <32362106+tkiyo86@users.noreply.github.com>
2021-07-06 00:13:42 +0900

    35_complete_button 2段目の完了ボタンを押すと3段目にテキスト＋「戻す」ボタンを生成 次で「戻す」機能を実装

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./styles.css";
  * 1,最初からすべてを実装しない、まずはアラート
  * htmlのbuttonにid=add-button を割り当てる
  * add-button に大してJS側で操作できる
-
+^
 const onClickAdd = () => {
   alert();
 };
@@ -63,12 +63,34 @@ const onClickAdd = () => {
   li.innerText = inputText;
   //console.log(li);
 
-  //33,bottonタグ生成
+  //33,botton完了タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   //33-2、イベントを追加する
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    deleteFromIncompleteList(completeButton.parentNode);
+    //const deleteTarget = completeButton.parentNode;
+    //console.log(deleteTarget);
+    //document.getElementById("incomplete-list").removeChild(deleteTarget);
+
+    // 完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText; // テキストを取得
+    // divを取得して中身を初期化する、その後テキストを入れる
+    addTarget.textContent = null;
+    // liタグ作成,コピペ
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    //divタグの子要素に各様を設定する
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton); // 35戻すボタン
+    console.log(addTarget);
+    // 35完了のリストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   //33,botton削除タグ生成
@@ -76,16 +98,27 @@ const onClickAdd = () => {
   deleteButton.innerText = "削除";
   //33-2、イベントを追加する
   deleteButton.addEventListener("click", () => {
-    //alert("削除");
-
     //34 削除ボタンを押すとHTMLのdivをまるごと削除したい
     // 押された削除ボタンの親タグ（div）を未完了リストから削除
 
     // divを取得,parentNode
-    const deleteTarget = deleteButton.parentNode;
-    // 取得したdivをincompletelistから削除する
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncompleteList(deleteButton.parentNode); // 以下3行を関数か
+    //const deleteTarget = deleteButton.parentNode;
+    //console.log(deleteTarget);
+    //document.getElementById("incomplete-list").removeChild(deleteTarget); // 取得したdivをincompletelistから削除する
   });
+
+  /**
+   * 35 完了ボタンを押すと3段目にDOMを生成して戻すボタンを生成する
+   * その後2段目は削除
+   * 戻すボタンの機能実装をする
+   */
+
+  // 未完了リストから指定の要素を削除
+  const deleteFromIncompleteList = (target) => {
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(target);
+  };
 
   // divタグの子要素に各要素を設定,HTMLをよく見る
   div.appendChild(li);
